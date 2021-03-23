880f8d51509368a0fda065fd8b3e8614ba263ffe
cstkyrilos <69175053+cstkyrilos@users.noreply.github.com>
2021-03-23 22:36:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
- -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+from pip.req import parse_requirements
 import re, ast
 
-# get version from __version__ variable in erpnext/__init__.py
+# get version from __version__ variable in cheque_management/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('erpnext/__init__.py', 'rb') as f:
+with open('cheque_management/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
 requirements = parse_requirements("requirements.txt", session="")
 
 setup(
-	name='erpnext',
+	name='cheque_management',
 	version=version,
-	description='Open Source ERP',
-	author='Frappe Technologies',
-	author_email='info@erpnext.com',
+	description='For managing receivable and payable cheques',
+	author='Direction',
+	author_email='info@egdirection.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
